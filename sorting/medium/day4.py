2430c865f207b514577a89edcef3742772b153ed
vaniltonpaulo <V.Paulo@campus.lmu.de>
2026-06-14 23:42:27 +0200

    more done

--- sorting/medium/day4.py
+++ sorting/medium/day4.py
@@ -33,3 +33,24 @@ Test.assert_equals(sort_it([[3], 7, [9], [5], 1, 6]), [1, [3], [5], 6, 7, [9]])
 Test.assert_equals(sort_it([[3], 7, [9], [5], 1, 6, [0]]), [[0], 1, [3], [5], 6, 7, [9]])
 
 Test.summary()
+
+def correct_sentences(s):
+    words = s.split()
+    sentences = []
+    current = []
+
+    for word in words:
+        if word[0].isupper() and current:
+            sentences.append(" ".join(current))
+            current = [word]
+        else:
+            current.append(word)
+
+    sentences.append(" ".join(current))
+
+    return ". ".join(sentence[0].upper() + sentence[1:] for sentence in sentences) + "."
+Test.assert_equals(correct_sentences("  mubashir loves  edabit  Matt  loves  edabit  "), "Mubashir loves edabit. Matt loves edabit.")
+Test.assert_equals(correct_sentences("  he is an engineer He sleeps a lot"), "He is an engineer. He sleeps a lot.")
+Test.assert_equals(correct_sentences(" his english is not good Help him     Thank you"), "His english is not good. Help him. Thank you.")
+Test.assert_equals(correct_sentences(" he is ready to join   airforce  Waiting for the  final approval"), "He is ready to join airforce. Waiting for the final approval.")
+Test.summary()
