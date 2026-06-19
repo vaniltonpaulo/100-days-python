f3668e881001405626b4c15e3577ce9ee62fe495
vaniltonpaulo <V.Paulo@campus.lmu.de>
2026-06-19 15:29:57 +0200

    more done

--- algorithm/very easy/day1.py
+++ algorithm/very easy/day1.py
@@ -135,3 +135,112 @@ Test.assert_equals(add_up(999), 499500)
 Test.assert_equals(add_up(175), 15400)
 Test.assert_equals(add_up(111), 6216)
 Test.summary()
+
+
+
+
+def programmers(one, two, three):
+    return max(one, two, three) - min(one,two, three)
+
+
+Test.assert_equals(programmers(1, 5, 9), 8)
+Test.assert_equals(programmers(43, 33, 43), 10)
+Test.assert_equals(programmers(88, 14, 23), 74)
+Test.assert_equals(programmers(33, 72, 74), 41)
+Test.assert_equals(programmers(147, 33, 526), 493)
+Test.assert_equals(programmers(234, 345, 457), 223)
+
+
+def add_binary(a, b):
+    return bin(a + b)[2:]
+
+
+
+Test.assert_equals(add_binary(1,1), '10')
+Test.assert_equals(add_binary(1,2), '11')
+Test.assert_equals(add_binary(4,5),'1001')
+Test.assert_equals(add_binary(8,20),'11100')
+Test.assert_equals(add_binary(100,20),'1111000')
+Test.assert_equals(add_binary(40,50),'1011010')
+Test.assert_equals(add_binary(65,77),'10001110')
+Test.assert_equals(add_binary(40,50),'1011010')
+Test.assert_equals(add_binary(1,0),'1')
+
+def leap_year(year):
+    return (year % 400 == 0) or (year % 100 != 0 and year % 4 == 0)
+
+
+Test.assert_equals(leap_year(2004), True)
+Test.assert_equals(leap_year(8), True)
+Test.assert_equals(leap_year(4), True)
+Test.assert_equals(leap_year(2019), False)
+Test.assert_equals(leap_year(1970), False)
+Test.assert_equals(leap_year(2021), False)
+Test.assert_equals(leap_year(1934), False)
+Test.assert_equals(leap_year(1874), False)
+Test.assert_equals(leap_year(1968), True)
+Test.assert_equals(leap_year(2024), True)
+Test.assert_equals(leap_year(1900), False)
+Test.assert_equals(leap_year(2100), False)
+Test.assert_equals(leap_year(2200), False)
+Test.summary()
+
+
+def sum_cubes(n):
+    total = 0
+    for i in range(1, n+1):
+        total += i ** 3
+    return total
+
+
+Test.assert_equals(sum_cubes(1), 1)
+Test.assert_equals(sum_cubes(2), 9)
+Test.assert_equals(sum_cubes(3), 36)
+Test.assert_equals(sum_cubes(4), 100)
+Test.assert_equals(sum_cubes(5), 225)
+Test.assert_equals(sum_cubes(6), 441)
+Test.assert_equals(sum_cubes(7), 784)
+Test.assert_equals(sum_cubes(8), 1296)
+Test.assert_equals(sum_cubes(9), 2025)
+Test.assert_equals(sum_cubes(10), 3025)
+Test.assert_equals(sum_cubes(123), 58155876)
+Test.assert_equals(sum_cubes(125), 62015625)
+Test.assert_equals(sum_cubes(133), 79405921)
+Test.assert_equals(sum_cubes(167), 196784784)
+Test.assert_equals(sum_cubes(188), 315630756)
+Test.assert_equals(sum_cubes(199), 396010000)
+Test.assert_equals(sum_cubes(200), 404010000)
+Test.assert_equals(sum_cubes(300), 2038522500)
+Test.assert_equals(sum_cubes(400), 6432040000)
+Test.assert_equals(sum_cubes(500), 15687562500)
+Test.assert_equals(sum_cubes(12345), 5807306426319225)
+
+
+
+def total_cups(n):
+    return n + (n//6)
+
+
+
+Test.assert_equals(total_cups(6), 7)
+Test.assert_equals(total_cups(3), 3)
+Test.assert_equals(total_cups(7), 8)
+Test.assert_equals(total_cups(12), 14)
+Test.assert_equals(total_cups(213), 248)
+Test.assert_equals(total_cups(16), 18)
+Test.summary()
+
+
+def largest_numbers(n, lst):
+    if n  == 0:
+        return []
+    return sorted(lst)[-n:]
+
+
+Test.assert_equals(largest_numbers(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]), [9, 10])
+Test.assert_equals(largest_numbers(3, [5, 1, 5, 2, 3, 1, 2, 3, 5]), [5, 5, 5])
+Test.assert_equals(largest_numbers(7, [9, 1, 50, 22, 3, 13, 2, 63, 5]), [3, 5, 9, 13, 22, 50, 63])
+Test.assert_equals(largest_numbers(0, [1, 2, 3, 4, 8, 7, 6, 5]), [])
+Test.assert_equals(largest_numbers(2, [4, 3, 2, 1]), [3, 4])
+Test.assert_equals(largest_numbers(1, [7, 19, 4, 2]), [19])
+Test.assert_equals(largest_numbers(3, [14, 12, 57, 11, 18, 16]), [16, 18, 57])
