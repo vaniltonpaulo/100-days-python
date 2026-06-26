be458c0480b3fa32bb7ecc1b93df837622c29105
vaniltonpaulo <V.Paulo@campus.lmu.de>
2026-06-26 14:43:12 +0200

    more done

---
+++ OOP/day1.py
@@ -0,0 +1,8 @@
+class House:
+    def __init__(self,wall_area):
+        self.wall_area = wall_area
+
+class Paint:
+    def __init__(self,buckets,color):
+        self.buckets = buckets
+        self.color = color
