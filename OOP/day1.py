cfe2fac50078b1eff0befd554f8c8601371333d4
vaniltonpaulo <V.Paulo@campus.lmu.de>
2026-06-28 14:01:43 +0200

    more done

--- OOP/day1.py
+++ OOP/day1.py
@@ -2,7 +2,16 @@ class House:
     def __init__(self,wall_area):
         self.wall_area = wall_area
 
+    def paint_needed(self,wall_area):
+        return self.wall_area * 2.5
+
 class Paint:
     def __init__(self,buckets,color):
         self.buckets = buckets
         self.color = color
+    
+    def total_price(self):
+        if self.color == "white":
+            return self.buckets * 1.99
+        else:
+            return self.buckets * 2.19
