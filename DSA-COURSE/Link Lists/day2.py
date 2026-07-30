b66d12efbb3d0781c0417356ed45721576ec7413
vaniltonpaulo <V.Paulo@campus.lmu.de>
2026-07-30 14:24:34 +0200

    more done

--- DSA-COURSE/Link Lists/day2.py
+++ DSA-COURSE/Link Lists/day2.py
@@ -10,7 +10,23 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
-    
+
+    def print_list(self):
+        temp = self.head 
+        while temp is not  None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self,value):
+        new_node  = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
 
 
 
